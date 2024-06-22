4c5cffef8997459ade2e4c7c6af91de04024d326
Jack <github@jackgaino.com>
2024-06-22 08:22:18 -0400

    Add hot reload for widgets

--- grydgets/widgets/widgets.py
+++ grydgets/widgets/widgets.py
@@ -10,102 +10,101 @@ import grydgets.widgets.text
 import grydgets.widgets.containers
 import grydgets.widgets.notifiable
 
-_name_to_widget_map = {}
 
+class WidgetManager:
+    def __init__(self):
+        self._name_to_widget_map = {}
+        self.name_to_instance = {}
+        self.map_all_the_widgets_in_here()
 
-def window(seq, n=2):
-    """Returns a sliding window (of width n) over data from the iterable
-    s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...
-    """
-    it = iter(seq)
-    result = tuple(itertools.islice(it, n))
-    if len(result) == n:
-        yield result
-    for elem in it:
-        result = result[1:] + (elem,)
-        yield result
+    def window(self, seq, n=2):
+        """Returns a sliding window (of width n) over data from the iterable
+        s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...
+        """
+        it = iter(seq)
+        result = tuple(itertools.islice(it, n))
+        if len(result) == n:
+            yield result
+        for elem in it:
+            result = result[1:] + (elem,)
+            yield result
 
+    def map_all_the_widgets_in_here(self):
+        all_widget_members = []
+        for module in ["containers", "image", "notifiable", "text"]:
+            module_name = f"grydgets.widgets.{module}"
+            all_widget_members.extend(inspect.getmembers(sys.modules[module_name]))
+        for name, obj in all_widget_members:
+            if (
+                    inspect.isclass(obj)
+                    and issubclass(obj, Widget)
+                    and "Widget" in obj.__name__
+            ):
+                class_name = obj.__name__.split("Widget")[0].lower()
+                if class_name:
+                    self._name_to_widget_map[class_name] = obj
 
-def map_all_the_widgets_in_here():
-    all_widget_members = []
-    for module in ["containers", "image", "notifiable", "text"]:
-        module_name = f"grydgets.widgets.{module}"
-        all_widget_members.extend(inspect.getmembers(sys.modules[module_name]))
-    for name, obj in all_widget_members:
-        if (
-            inspect.isclass(obj)
-            and issubclass(obj, Widget)
-            and "Widget" in obj.__name__
-        ):
-            class_name = obj.__name__.split("Widget")[0].lower()
-            if class_name:
-                _name_to_widget_map[class_name] = obj
+    def create_widget_tree(self, widget_dictionary, path=None, counter=None):
+        if path is None:
+            path = []
+        if counter is None:
+            counter = {}
 
+        widget_type_name = widget_dictionary["widget"]
+        widget_name = widget_dictionary.get("name") or widget_type_name
+        # Increment or initialize the widget counter
+        counter[widget_name] = counter.get(widget_name, 0) + 1
 
-name_to_instance = {}
-
-
-def create_widget_tree(widget_dictionary, path=None, counter=None):
-    global name_to_instance
-
-    if path is None:
-        path = []
-    if counter is None:
-        counter = {}
-
-    widget_type_name = widget_dictionary["widget"]
-    widget_name = widget_dictionary.get("name") or widget_type_name
-    # Increment or initialize the widget counter
-    counter[widget_name] = counter.get(widget_name, 0) + 1
-
-    # Generate a unique name based on the path and counter
-    unique_name = "_".join(path + [f"{widget_name}{counter[widget_name]}"])
-    widget_parameters = {
-        key: value
-        for key, value in widget_dictionary.items()
-        if key not in ["widget", "children"]
-    }
-    logging.debug(f"Adding to widget tree: {unique_name}")
-    # Pass the unique name as an extra parameter
-    widget_parameters["unique_name"] = unique_name
-    widget = _name_to_widget_map[widget_type_name](**widget_parameters)
-    if hasattr(widget, "notify"):
-        if callable(widget.notify):
-            if widget_name not in name_to_instance:
-                name_to_instance[widget_name] = widget
+        # Generate a unique name based on the path and counter
+        unique_name = "_".join(path + [f"{widget_name}{counter[widget_name]}"])
+        widget_parameters = {
+            key: value
+            for key, value in widget_dictionary.items()
+            if key not in ["widget", "children"]
+        }
+        logging.debug(f"Adding to widget tree: {unique_name}")
+        # Pass the unique name as an extra parameter
+        widget_parameters["unique_name"] = unique_name
+        widget = self._name_to_widget_map[widget_type_name](**widget_parameters)
+        if hasattr(widget, "notify"):
+            if callable(widget.notify):
+                if widget_name not in self.name_to_instance:
+                    self.name_to_instance[widget_name] = widget
+                else:
+                    logging.warning(
+                        f"Warning: Duplicate widget name '{widget_name}'. "
+                        "Not adding to list of notifiable widgets."
+                    )
             else:
                 logging.warning(
-                    f"Warning: Duplicate widget name '{widget_name}'. "
-                    "Not adding to list of notifiable widgets."
+                    f"Warning: 'notify' attribute of widget '{widget_name}' "
+                    f"is not callable. Skipping."
                 )
-        else:
-            logging.warning(
-                f"Warning: 'notify' attribute of widget '{widget_name}' "
-                f"is not callable. Skipping."
-            )
 
-    if "children" in widget_dictionary:
-        child_counter = {}  # Reset counter for children
-        for child in widget_dictionary["children"]:
-            widget.add_widget(
-                create_widget_tree(
-                    child,
-                    path + [f"{widget_name}{counter[widget_name]}"],
-                    child_counter,
+        if "children" in widget_dictionary:
+            child_counter = {}  # Reset counter for children
+            for child in widget_dictionary["children"]:
+                widget.add_widget(
+                    self.create_widget_tree(
+                        child,
+                        path + [f"{widget_name}{counter[widget_name]}"],
+                        child_counter,
+                    )
                 )
-            )
 
-    return widget
+        return widget
 
+    def recursively_stop_widgets(self, main_widget):
+        if isinstance(main_widget, ContainerWidget):
+            for widget in main_widget.widget_list:
+                logging.debug(f"Going deeper in {main_widget}")
+                self.stop_all_widgets(widget)
+        elif isinstance(main_widget, UpdaterWidget):
+            logging.debug(f"Stopping UpdaterWidget {main_widget}")
+            main_widget.stop()
 
-def stop_all_widgets(main_widget):
-    if isinstance(main_widget, ContainerWidget):
-        for widget in main_widget.widget_list:
-            logging.debug(f"Going deeper in {main_widget}")
-            stop_all_widgets(widget)
-    elif isinstance(main_widget, UpdaterWidget):
-        logging.debug(f"Stopping UpdaterWidget {main_widget}")
-        main_widget.stop()
+        del main_widget
 
-
-map_all_the_widgets_in_here()
+    def stop_all_widgets(self, main_widget):
+        self.recursively_stop_widgets(main_widget)
+        self.name_to_instance = {}
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-
+import signal
 import pygame
 from pygame.transform import rotate
 import logging
@@ -9,17 +9,18 @@ from flask import Flask, request, jsonify
 
 from grydgets import config
 from grydgets.widgets.containers import ScreenWidget
-from grydgets.widgets.widgets import (
-    create_widget_tree,
-    stop_all_widgets,
-    name_to_instance,
-)
+from grydgets.widgets.widgets import WidgetManager
 
 logging.basicConfig(
     format="[%(asctime)s] %(levelname)s:%(name)s:%(message)s", level=logging.DEBUG
 )
 
-widget_tree = config.load_yaml("widgets.yaml")
+
+def load_widget_tree():
+    return config.load_yaml("widgets.yaml")
+
+
+widget_tree = load_widget_tree()
 conf = config.load_config("conf.yaml")
 
 if "fb-device" in conf["graphics"]:
@@ -40,6 +41,7 @@ if conf["graphics"]["fullscreen"]:
 logging.getLogger().setLevel(logging.getLevelName(conf["logging"]["level"].upper()))
 
 stop_everything = threading.Event()
+reload_lock = threading.Lock()
 
 pygame.init()
 pygame.mixer.quit()
@@ -50,6 +52,8 @@ screen_size = tuple(conf["graphics"]["resolution"])
 screen = pygame.display.set_mode(screen_size, pygame_flags)
 pygame.display.set_caption("Grydgets dashboard", "Grydgets")
 
+widget_manager = WidgetManager()
+
 screen_widget = ScreenWidget(
     screen_size,
     image_path=widget_tree.get("background_image", None),
@@ -57,7 +61,7 @@ screen_widget = ScreenWidget(
     drop_shadow=widget_tree.get("drop_shadow", False),
 )
 
-screen_widget.add_widget(create_widget_tree(widget_tree["widgets"][0]))
+screen_widget.add_widget(widget_manager.create_widget_tree(widget_tree["widgets"][0]))
 
 app = Flask(__name__)
 
@@ -66,10 +70,10 @@ app = Flask(__name__)
 def widget():
     payload = request.get_json()
     requested_widget = payload["widget"]
-    if requested_widget not in name_to_instance:
+    if requested_widget not in widget_manager.name_to_instance:
         return jsonify({"success": False, "error": "Widget not found"}), 400
 
-    name_to_instance[requested_widget].notify(payload)
+    widget_manager.name_to_instance[requested_widget].notify(payload)
     return jsonify({"success": True})
 
 
@@ -81,6 +85,30 @@ server_thread = threading.Thread(target=run_server)
 server_thread.daemon = True
 server_thread.start()
 
+
+def reload_configuration(signum, frame):
+    global screen_widget, widget_tree
+    logging.info("Reloading configuration...")
+    with reload_lock:
+        try:
+            new_widget_tree = load_widget_tree()
+            logging.info("Stopping all widgets...")
+            widget_manager.stop_all_widgets(screen_widget)
+            screen_widget = ScreenWidget(
+                screen_size,
+                image_path=new_widget_tree.get("background_image", None),
+                color=new_widget_tree.get("background_color", (0, 0, 0)),
+                drop_shadow=new_widget_tree.get("drop_shadow", False),
+            )
+            screen_widget.add_widget(widget_manager.create_widget_tree(new_widget_tree["widgets"][0]))
+            widget_tree = new_widget_tree
+            logging.info("Configuration reloaded successfully.")
+        except Exception as e:
+            logging.error(f"Failed to reload configuration: {e}")
+
+
+signal.signal(signal.SIGUSR1, reload_configuration)
+
 fps_time = time.time()
 frame_data = list()
 last_screen_surface = None
@@ -91,16 +119,17 @@ while not stop_everything.is_set():
             if event.type in [pygame.QUIT, pygame.MOUSEBUTTONDOWN]:
                 stop_everything.set()
 
-        screen_widget.tick()
-        if not last_screen_surface or screen_widget.is_dirty():
-            if conf["graphics"].get("flip", False):
-                last_screen_surface = rotate(screen_widget.render(screen_size), 180)
-            else:
-                last_screen_surface = screen_widget.render(screen_size)
+        with reload_lock:
+            screen_widget.tick()
+            if not last_screen_surface or screen_widget.is_dirty():
+                if conf["graphics"].get("flip", False):
+                    last_screen_surface = rotate(screen_widget.render(screen_size), 180)
+                else:
+                    last_screen_surface = screen_widget.render(screen_size)
 
-            screen.blit(last_screen_surface, (0, 0))
+                screen.blit(last_screen_surface, (0, 0))
 
-            pygame.display.flip()
+                pygame.display.flip()
 
         sleep_time = max((1 / fps_limit) - (time.time() - frame_start), 0)
         # if sleep_time == 0:
@@ -116,5 +145,5 @@ while not stop_everything.is_set():
         fps_time = time.time()
         frame_data = list()
 
-stop_all_widgets(screen_widget)
+widget_manager.stop_all_widgets(screen_widget)
 pygame.quit()
